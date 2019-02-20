63e35a6d9208256d5007e4019bf02967f67cfd38
凉之 <zhaodong.xzd@alibaba-inc.com>
2019-02-20 22:46:35 +0800

    create snowflake

---
+++ sharding-core/src/main/java/io/shardingsphere/core/keygen/leaf/SnowFlakeKeyGenerator.java
@@ -0,0 +1,10 @@
+package io.shardingsphere.core.keygen.leaf;
+
+import io.shardingsphere.core.keygen.KeyGenerator;
+
+public class SnowFlakeKeyGenerator implements KeyGenerator {
+    @Override
+    public Number generateKey() {
+        return null;
+    }
+}
